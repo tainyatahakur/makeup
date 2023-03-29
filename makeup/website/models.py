bacb5da2a6ddd3cc3838c227bec1fed8c52ffe5a
tainya <thakurtainya@gmail.com>
2023-03-29 18:08:24 +0530

    yes

--- makeup/website/models.py
+++ makeup/website/models.py
@@ -10,18 +10,22 @@ class ContactModel(models.Model):
         return self.fname
     
 class BookingModel(models.Model):
-    username = models.CharField(max_length=50)
+    # username = models.CharField(max_length=50)
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
     phone = models.IntegerField()
-    phone = models.IntegerField(blank=True, null=True) 
     email = models.EmailField()
-    prices = models.IntegerField()                                            
-    services = models.CharField(max_length=50)                             
+    #prices = models.IntegerField()                                            
+    services = models.CharField(max_length=50) 
+    state = models.CharField(max_length=50) 
+    city = models.CharField(max_length=50)
+    zip = models.IntegerField()
+    datentime = models.DateField()                         
     # contact = models.ForeignKey("ContactModel", on_delete=models.CASCADE)        
     
     def __str__(self):
-        return self.username
+        return self.fname
     
 
 class ServicesModel(models.Model):
